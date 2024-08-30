724d395ba78eca49444d336e191c5e2331e81f31
soos <92793904+MirrorPI@users.noreply.github.com>
2024-08-30 17:30:11 +0900

    Update InsertionSort.cpp

--- cppAlgorithm/InsertionSort.cpp
+++ cppAlgorithm/InsertionSort.cpp
@@ -9,11 +9,16 @@ using namespace std;
 
 void InsertionSort(vector <int>& arr){
     int n = arr.size();
-    int j;
+
     for(int i=1; i<n; ++i){
         int key = arr[i];
-        for(j=i-1; j>=0 && arr[j]>key; --j){
+        // for(j=i-1; j>=0 && arr[j]>key; --j){
+        //     arr[j+1] = arr[j];
+        // }
+        int j = i-1;
+        while(j>=0 && arr[j]>key){
             arr[j+1] = arr[j];
+            --j;
         }
         arr[j+1] = key;
     }
